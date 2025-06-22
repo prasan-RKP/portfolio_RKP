dbb62dc3c0dbc29f7180314070626529925fd9ed
Prasan RKP <prasan543e@gmail.com>
2025-06-22 17:49:03 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const _dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://portfolio-rkp.onrender.com"],
   credentials: true
 }));
 
