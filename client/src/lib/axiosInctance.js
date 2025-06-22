b01ca8230d7496db0b847aeb64e3be3950ca6f52
Prasan RKP <prasan543e@gmail.com>
2025-06-22 18:04:37 +0530

    added the deployed route

--- client/src/lib/axiosInctance.js
+++ client/src/lib/axiosInctance.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3008/auth',
+
+    //for production 
+    //baseURL: 'http://localhost:3008/auth',
+
+    //for all and global
+    baseURL: 'https://portfolio-rkp.onrender.com',
     withCredentials: false
 })
