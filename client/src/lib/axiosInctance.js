eed9ed8d54bb97012b25144819ff9c87dfd4acc4
Prasan RKP <prasan543e@gmail.com>
2025-06-22 21:54:57 +0530

    change saved

--- client/src/lib/axiosInctance.js
+++ client/src/lib/axiosInctance.js
@@ -1,28 +1,27 @@
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-
-//     //for production 
-//     baseURL: 'http://localhost:3008/auth',
-
-//     //for all and global
-//     //baseURL: 'https://portfolio-rkp.onrender.com/auth',
-//     withCredentials: false
-// })
-
-
 import axios from 'axios';
 
-// Dynamically choose the baseURL depending on environment
+export const axiosInstance = axios.create({
+
+    //for production 
+    //baseURL: 'http://localhost:3008/auth',
+
+    //for all and global
+    baseURL: 'https://portfolio-rkp.onrender.com/auth',
+    withCredentials: false
+})
+
+/*
+
+-> for development code
 const baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:3008/auth'
     : 'https://portfolio-rkp.onrender.com/auth';
 
-    console.log("Current Mode:", import.meta.env.MODE);
-console.log("Base URL:", baseURL);
-
 export const axiosInstance = axios.create({
   baseURL,
-  withCredentials: false, // Set to true if you're using cookies
+  withCredentials: false,
 });
+
+
+*/
